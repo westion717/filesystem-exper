f1a619d7c3ed3fcf148310318eab74dcb67cb5f8
Westion <westion717@gmail.com>
2015-01-09 18:58:09 +0800

    add function about flushing in buffer,inode,bitmap add testing for finding file

--- test/dir_entry/find_dir_test.c
+++ test/dir_entry/find_dir_test.c
@@ -1,12 +1,12 @@
 /*
  * =================================================
  *
- *       Filename:  add_dir_test.c
+ *       Filename:  test/dir_entry/find_dir_test.c
  *
  *    Description:  
  *
  *        Version:  0.01
- *        Created:  Thu Jan  8 21:28:23 2015
+ *        Created:  Fri Jan  9 16:43:09 2015
  *         Author:  westion, westion717@gmail.com
  *        Company:  ZJUT
  *
@@ -21,8 +21,8 @@
 #include "inode.h"
 #include "inode.h"
 
-
 int main(int argc, char** argv){
+
 
 	initFile("../../build/hd");
 	initDiskBuf();
@@ -31,28 +31,19 @@ int main(int argc, char** argv){
 	initRootDir();
 	initINode();
 
-	printDirInfo();
+
+	short inum=_findINodeByName("/home/me");	
+	const struct inode* node=findINode(inum);
+	printf("/home/me,mode=%d\n",node->i_mode);
+	printf("/home/me,subdir node num=%d\n",node->i_zone[0]);
+
+	short inum1 =_findINodeByName("/home/me/a.txt");	
+	const struct inode* node1=findINode(inum1);
+	printf("/home/me/a.txt,mode=%d\n",node1->i_mode);
+	printf("/home/me/a.txt,=%ld\n",node1->i_size);
 
 
-	printf("1:preparing for inserting\n");
-	_create_dir_entry("/home",MODE_DIR,1,1);
-	printf("1:inserted");
-	printDirInfo();
-
-
-	printf("2:preparing for inserting\n");
-	_create_dir_entry("/home/me",MODE_DIR,1,1);
-	printf("2:inserted");
-	printDirInfo();
-
-
-	printf("3:preparing for inserting\n");
-	_create_dir_entry("/home/me/a.txt",MODE_FILE,1,1);
-	printf("3:inserted");
-	printDirInfo();
 	releaseBitMap();
-
 	flushBuff();
-
-	return 0;
+    return 0;
 }
